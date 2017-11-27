39a7fdc4ae89ed630fb2589f57a2f7dd924c857f
TheLKL321 <lkl.zarebski@gmail.com>
2017-11-27 23:19:39 +0100

    import bal make and height from pSet and adapt

--- iSet.ml
+++ iSet.ml
@@ -41,9 +41,63 @@ let solver t str = 42
 let is_empty s =
   s.set = Empty
 
+(** Zwraca wysokość danego drzewa  *)
+let height = function
+  | Node (_, _, _, h) -> h
+  | Empty -> 0
+
+(** Złącza poddrzewa l i r podłączając je do nowego korzenia o wartości k  *)
+let make l k r =
+  Node (l, k, r, max (height l) (height r) + 1)
+
+(** Wykonuje rotacje aby drzewo pozostało wybalansowane  *)
+let bal l k r =
+  let hl = height l in
+  let hr = height r in
+  if hl > hr + 2 then
+    match l with
+    | Node (ll, lk, lr, _) ->
+        if height ll >= height lr then make ll lk (make lr k r)
+        else
+          match lr with
+          | Node (lrl, lrk, lrr, _) ->
+              make (make ll lk lrl) lrk (make lrr k r)
+          | Empty -> assert false
+    | Empty -> assert false
+  else if hr > hl + 2 then
+    match r with
+    | Node (rl, rk, rr, _) ->
+        if height rr >= height rl then make (make l k rl) rk rr
+        else
+          match rl with
+          | Node (rll, rlk, rlr, _) ->
+              make (make l k rll) rlk (make rlr rk rr)
+          | Empty -> assert false
+    | Empty -> assert false
+  else Node (l, k, r, max hl hr + 1)
+
+(** Używana przez add do dodawania (x, y) do danego setu  *)
+(** TODO: modify to include omnomnomnom cases  *)
+let rec add_one cmp (x, y) = function
+  | Node (l, k, r, h) ->
+      let c = cmp (x, y) k
+      in
+        if c = 0 then
+          Node (l, x, r, h)
+        else if c < 0 then
+          let nl = add_one cmp (x, y) l
+          in
+            bal nl k r
+        else
+          let nr = add_one cmp (x, y) r
+          in
+            bal l k nr
+  | Empty -> Node (Empty, (x, y), Empty, 1)
+
 (** Zwraca set będący wynikiem dodania elementów przedziału (x, y) do setu s
     x <= y    *)
-let add (x, y) s = 42
+let add (x, y) { cmp = cmp; set = set } =
+  { cmp = cmp; set = add_one cmp (x, y) set }
 
 (** Zwraca set będący wynikiem usunięcia z setu s elementów przedziału (x, y)
     x <= y    *)
